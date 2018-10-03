5c1e97dc5378382045e4530ea557aa17bfdee941
KimYoonho Aaron <youknow.yoonho@gmail.com>
2018-10-04 01:59:24 +0900

    update

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Hello Nodejs!!');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello, world!').end();
+});
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
+// [END gae_node_request_example]
